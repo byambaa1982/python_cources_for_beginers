12311a1b7a9397ac520656861e368e7d5c218ff2
Byamba Enkhbat <byambaa1982@gmail.com>
2023-05-16 14:01:13 -0500

    Update main.py

--- Mini Project 2/main.py
+++ Mini Project 2/main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', message='Hello, World!')
+
+@app.route('/form', methods=['GET', 'POST'])
+def render_form():
+    if request.method == 'POST':
+        text = request.form.get('text')
+        return render_template('index.html', message=text.lower())
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
