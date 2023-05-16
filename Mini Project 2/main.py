5ba2476d3314bec77b37f516250c1dc762452f8e
Byamba Enkhbat <byambaa1982@gmail.com>
2023-05-16 13:22:35 -0500

    Update main.py

--- Mini Project 2/main.py
+++ Mini Project 2/main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html', message='Hello, World!')
 
 if __name__ == '__main__':
     app.run(debug=True)
